a430083fdd536317c5c3bcdf924b214e36d7436c
Paystar-5000 <bschaefer4@cscc.edu>
2015-09-09 23:10:16 -0400

    The addition tests are done and pass

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,22 +5,39 @@ describe Calculator do
   let(:calc){ Calculator.new }
 
   describe "#add" do
-    it "returns an integer" do
-      expect(calc.add 1, 1).to be_an Integer
+
+    it "return an integer" do
+      expect(Calculator.new.add(1,1)).to be_an(Integer)
     end
 
-    context "when adding zero to an integer" do
-      it "returns the integer" do
-        expect(calc.add 4, 0).to eq 4
+    it "return the sum of its two arguments" do
+      expect(Calculator.new.add(1,1)).to eq(2)
+    end
+
+    context "when I add two numbers" do
+      it "returns the correct sum regardless of order" do
+        expect(calc.add(1,2)).to eq(calc.add(2,1))
+        expect(calc.add(1,2)).to eq(3)
       end
     end
 
-    it "is not dependent on the operation order" do
-      expect(calc.add 1, 2).to eq calc.add(2, 1)
+    context "when I add zero to an integer" do
+      it "returns the integer" do
+        expect(calc.add(5,0)).to eq(5)
+      end
     end
 
-    it "accepts more than 2 arguments" do
-      expect(calc.add 2, 4, 3).to eq 9
+    context "when I add an integer to itself" do
+      it "should double the integer" do
+        expect(calc.add(5,5)).to eq(5*2)
+      end
+    end
+
+    context "when I add more than two numbers" do
+      it "returns the correct sum" do
+        expect(calc.add(1,2,3)).to eq(6)
+        expect(calc.add(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)).to eq(120)
+      end
     end
   end
   
