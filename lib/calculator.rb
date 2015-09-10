e2c36c03e6b118c060693e4e34784e44c0003fac
Paystar-5000 <bschaefer4@cscc.edu>
2015-09-09 23:19:38 -0400

    The subtraction tests are done and pass

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -3,5 +3,9 @@ class Calculator
   def add *numbers
     numbers.inject(:+)
   end
+
+  def subtract *numbers
+    numbers.inject(:-)
+  end
   
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -40,5 +40,42 @@ describe Calculator do
       end
     end
   end
+
+    describe "#subtract" do
+
+    it "return an integer" do
+      expect(Calculator.new.subtract(5,4)).to be_an(Integer)
+    end
+
+    it "return the difference of its two arguments" do
+      expect(Calculator.new.subtract(4,2)).to eq(2)
+    end
+
+    context "when I subtract two numbers" do
+      it "returns the correct result dependent of order" do
+        expect(calc.subtract(1,2)).to eq(-1)
+        expect(calc.subtract(2,1)).to eq(1)
+      end
+    end
+
+    context "when I subtract zero from an integer" do
+      it "returns the integer" do
+        expect(calc.subtract(5,0)).to eq(5)
+      end
+    end
+
+    context "when I subtract an integer from itself" do
+      it "should equal zero" do
+        expect(calc.subtract(5,5)).to eq(0)
+      end
+    end
+
+    context "when I subtract more than two numbers" do
+      it "returns the correct result" do
+        expect(calc.subtract(10,2,3)).to eq(5)
+        expect(calc.subtract(100,2,3,4,5,6,7,8,9,10,11,12,13)).to eq(10)
+      end
+    end
+  end
   
 end
